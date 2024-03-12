0239e9538f5bf8c758b4a9dd30f734e27c7695e1
Willy Cotes <willycotes@gmail.com>
2024-03-12 06:43:24 -0500

    function validate formated input componente

---
+++ build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n', 'wp-primitives'), 'version' => '90dcdf412f1089bd3f3b');
---
+++ build/view.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => '53fcdfc25c5c1ef41153');
