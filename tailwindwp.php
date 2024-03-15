80d7bba22abc2927bc8891496ee753247a820115
Willy Cotes <willycotes@gmail.com>
2024-03-15 07:09:56 -0500

    modified main file, enqueue scripts aplication

--- tailwindwp.php
+++ tailwindwp.php
@@ -18,3 +18,13 @@
 if ( ! defined( 'ABSPATH' ) ) {
 	exit; // Exit if accessed directly.
 }
+
+/**
+ * Enqueue script 
+ */
+function cws_enqueue_editor_scripts_toobar_and_sidebar() {
+	$asset_file = include plugin_dir_path( __FILE__ ) . '/build/index.asset.php';
+	wp_enqueue_script('tailwindwp', plugin_dir_url( __FILE__ ) . '/build/index.js', $asset_file['dependencies'], $asset_file['version'], true)
+}
+
+add_action( 'enqueue_block_editor_assets', __NAMESPACE__ . '\cws_enqueue_editor_scripts_toobar_and_sidebar' );
