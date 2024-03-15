f79722b679e87d7199ca3c4cf7077c1c4de51ba0
Willy Cotes <willycotes@gmail.com>
2024-03-15 06:51:12 -0500

    change name and namespace definition

---
+++ tailwindwp.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Plugin Name:       TailwindWP
+ * Description:       Tailwind integration in WordPress block editor.
+ * Requires at least: 6.1
+ * Requires PHP:      7.0
+ * Version:           0.1.0
+ * Author:            CotesWebStudio
+ * License:           GPL-2.0-or-later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain:       tailwindwp
+ *
+ * @package           create-block
+ */
+
+ namespace tailwindwp;
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly.
+}
