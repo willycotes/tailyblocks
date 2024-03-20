2c29f005fabbdc063bebbd3f38996a609f781e46
Willy Cotes <willycotes@gmail.com>
2024-03-20 08:03:53 -0500

    ...

--- src/index.js
+++ src/index.js
@@ -16,6 +16,13 @@ import { createHigherOrderComponent } from "@wordpress/compose";
  * @see https://www.npmjs.com/package/@wordpress/scripts#using-css
  */
 import "./style.scss";
+/**
+ * Lets webpack process CSS, SASS or SCSS files referenced in JavaScript files.
+ * Those files can contain any CSS code that gets applied to the editor.
+ *
+ * @see https://www.npmjs.com/package/@wordpress/scripts#using-css
+ */
+import "./editor.scss";
 
 /**
  * Internal dependencies
--- tailwindwp.php
+++ tailwindwp.php
@@ -20,77 +20,96 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 /**
- * Enqueue JavaScript toolbar and sidebar control in block editor
+ * Currently plugin version.
+ * Start at version 0.1.0 and use SemVer - https://semver.org
+ * Rename this for your plugin and update it as you release new versions.
  */
-function cws_enqueue_editor_scripts_toolbar_and_sidebar() {
-	$asset_file = include plugin_dir_path( __FILE__ ) . 'build/index.asset.php';
-	\wp_enqueue_script('tailwindwp', plugin_dir_url( __FILE__ ) . 'build/index.js', $asset_file['dependencies'], $asset_file['version'], true);
-}
+define( 'PLUGIN_NAME_VERSION', '0.1.0' );
 
-\add_action( 'enqueue_block_editor_assets', __NAMESPACE__ . '\cws_enqueue_editor_scripts_toolbar_and_sidebar', 20 );
+/**
+ * Plugins constants path and url root directory
+ */
+define('TAILWINDWP_URL', plugin_dir_url( __FILE__ ));
+// define('TAILWINDWP_PATH', plugin_dir_path( __FILE__ ));
+define('TAILWINDWP_PATH', plugin_basename( __FILE__ ));
+echo WP_CONTENT_DIR;
+
+echo "<p>" . TAILWINDWP_URL . "</p>";
+echo "<p>" . TAILWINDWP_PATH . "</p>";
 
 /**
  * Enqueue JavaScript toolbar and sidebar control in block editor
  */
-function cws_enqueue_editor_styles_toolbar_and_sidebar() {
-	\wp_enqueue_style('tailwindwp', plugin_dir_url( __FILE__ ) . 'build/index.css');
+function enqueue_editor_scripts_toolbar_and_sidebar() {
+	$asset_file = include TAILWINDWP_PATH . 'build/index.asset.php';
+	\wp_enqueue_script('tailwindwp', TAILWINDWP_URL . 'build/index.js', $asset_file['dependencies'], $asset_file['version'], true);
 }
 
-\add_action( 'enqueue_block_editor_assets', __NAMESPACE__ . '\cws_enqueue_editor_styles_toolbar_and_sidebar', 20 );
+\add_action( 'enqueue_block_editor_assets', __NAMESPACE__ . '\enqueue_editor_scripts_toolbar_and_sidebar', 20 );
 
 /**
- * Enqueue tailwind CDN in editor and frontend
+ * Enqueue Styles toolbar and sidebar control in block editor
  */
- function cws_enqueue_tailwind_cdn() {
-	// Simulando get_option() Opción definida desde el editor
-	$is_tailwind_CDN = true;
-	// -----------------------------------------------------
+function enqueue_editor_styles_toolbar_and_sidebar() {
+	\wp_enqueue_style('tailwindwp', TAILWINDWP_URL . 'build/index.css');
+}
 
-	if ( !$is_tailwind_CDN ) {
+\add_action( 'enqueue_block_editor_assets', __NAMESPACE__ . '\enqueue_editor_styles_toolbar_and_sidebar', 20 );
+
+/**
+ * Enqueue tailwind CDN only in editor
+ */
+function enqueue_tailwind_cdn() {
+	if (! is_admin()) {
 		return;
 	}
-	$tailwindCDN = 'https://cdn.tailwindcss.com';
+
+	$tailwind_CDN = 'https://cdn.tailwindcss.com';
 	
-	\wp_register_script( 'tailwind_cdn', $tailwindCDN, array(), true, false );
+	\wp_register_script( 'tailwind_cdn', $tailwind_CDN, array(), true, false );
 
 	\wp_enqueue_script( 'tailwind_cdn' );
- }
+}
 
- \add_action( 'enqueue_block_assets', __NAMESPACE__ . '\cws_enqueue_tailwind_cdn', 10 );
+\add_action( 'enqueue_block_assets', __NAMESPACE__ . '\enqueue_tailwind_cdn', 10 );
 
 
 /**
  * Enqueue Tailwind configuration in editor
  */
- function cws_tailwind_config() {
-	// Simulando get_option() Opción definida desde el editor
-	$is_tailwind_CDN = true;
-	// -------------------------------------------------------
-
-	if ( !$is_tailwind_CDN ) {
+function enqueue_tailwind_cdn_config() {
+	if (! is_admin(  )) {
 		return;
 	}
-	\wp_register_script('tailwind_config', plugin_dir_url( __FILE__ ) . 'build/tailwind-cdn.config.js', array('tailwind_cdn'), true, false);
 
-	\wp_enqueue_script( 'tailwind_config' );
- }
- \add_action('enqueue_block_assets', __NAMESPACE__ . '\cws_tailwind_config');
+	\wp_register_script('tailwind_cdn_config', TAILWINDWP_URL . 'build/tailwind-cdn.config.js', array('tailwind_cdn'), true, false);
+
+	\wp_enqueue_script( 'tailwind_cdn_config' );
+}
+
+\add_action('enqueue_block_assets', __NAMESPACE__ . '\enqueue_tailwind_cdn_config');
 
  /**
  * Enqueue tailwind style generated in editor and frontend
  */
- function enqueue_tailwind_style_generated() {
-	// Simulando get_option() Opción definida desde el editor
-	$is_tailwind_CDN = true;
-	//-------------------------------------------------------
-
-	if ( $is_tailwind_CDN ) {
-		return;
-	}
+// function enqueue_style_tailwind_generated() {
 	
-	\wp_register_script( 'tailwind_styles_generated', plugin_dir_url( __FILE__ ) . 'build/style-index.css', array(), true, false );
+// 	\wp_register_style( 'tailwind_styles_generated', TAILWINDWP_URL . 'build/style-index.css', array(), true, false );
 
-	\wp_enqueue_script( 'tailwind_styles_generated' );
- }
+// 	\wp_enqueue_style( 'tailwind_styles_generated' );
+// }
 
- \add_action( 'enqueue_block_assets', __NAMESPACE__ . '\enqueue_tailwind_style_generated', 10 );
+// \add_action( 'enqueue_block_assets', __NAMESPACE__ . '\enqueue_tailwind_style_generated', 10 );
+
+
+/**
+ * ./bin/tailwindcss-linux-x64 -i src/style.scss -o src/styles/output.css
+ */
+require_once TAILWINDWP_PATH . 'includes/class-tailwind-controller.php';
+// support wp-now
+$tailwindExecutable = '/var/www/practices/wordpress-practices/gutenberg-blocks/addclassblock/bin/tailwindcss-linux-x64';
+$id = 1;
+$content = '<div class="text-lg font-bold">¡Hola, mundo!</div>';
+
+$tailwind_controller = new \Tailwind_controller($tailwindExecutable, $id);
+$tailwindCSS = $tailwind_controller->generateTailwindCSS($content);
